bf53dfbaf75e169719f3210741d9af02b197ae7b
akosgarai <szlem01@gmail.com>
2016-06-25 20:34:58 +0200

    Init function - setup provider, activity objects - tested

--- js/authoritysearch.js
+++ js/authoritysearch.js
@@ -1,3 +1,45 @@
 angular.module('authoritysearch', []).controller('authoritysearchController', ['$scope', function ($scope) {
+    $scope.providers = [];
+    $scope.activities = [];
+
+    $scope.init = function () {
+        $scope.providers = [
+            {
+                'label' : 'Provider1 label',
+                'id' : 1,
+                'status' : 'default',
+                'activityList' : [3]
+            },
+            {
+                'label' : 'Provider2 label',
+                'id' : 2,
+                'status' : 'default',
+                'activityList' : []
+            }
+        ];
+        $scope.activities = [
+            {
+                'label' : 'activity label 1',
+                'id' : 1,
+                'hiddenFlag' : false,
+                'displayCount' : 0,
+                'childActivity' : []
+            },
+            {
+                'label' : 'activity label 2',
+                'id' : 2,
+                'hiddenFlag' : false,
+                'displayCount' : 0,
+                'childActivity' : []
+            },
+            {
+                'label' : 'hidden activity label',
+                'id' : 3,
+                'hiddenFlag' : true,
+                'displayCount' : 0,
+                'childActivity' : []
+            }
+        ];
+    };
 }]);
 
--- test/authoritysearch-test.js
+++ test/authoritysearch-test.js
@@ -17,4 +17,51 @@ describe('Example module Unit Tests', function () {
             expect($scope.expected).toEqual(expected); 
         }); 
     });
+    describe('TEST002 - init function test', function () {
+        it('it checks the init function - mock the init request for providers and activities', function () {
+            var $scope = {};
+            var controller = $controller('authoritysearchController', { '$scope' : $scope});
+            $scope.providers = [];
+            $scope.activities = [];
+            var expectedProviders = [
+                {
+                    'label' : 'Provider1 label',
+                    'id' : 1,
+                    'status' : 'default',
+                    'activityList' : [3]
+                },
+                {
+                    'label' : 'Provider2 label',
+                    'id' : 2,
+                    'status' : 'default',
+                    'activityList' : []
+                }
+            ];
+            var expectedActivities = [
+                {
+                    'label' : 'activity label 1',
+                    'id' : 1,
+                    'hiddenFlag' : false,
+                    'displayCount' : 0,
+                    'childActivity' : []
+                },
+                {
+                    'label' : 'activity label 2',
+                    'id' : 2,
+                    'hiddenFlag' : false,
+                    'displayCount' : 0,
+                    'childActivity' : []
+                },
+                {
+                    'label' : 'hidden activity label',
+                    'id' : 3,
+                    'hiddenFlag' : true,
+                    'displayCount' : 0,
+                    'childActivity' : []
+                }
+            ];
+            $scope.init();
+            expect([$scope.providers, $scope.activities]).toEqual([expectedProviders, expectedActivities]);
+        });
+    });
 });
